6963dacf86f920ff0bff01ec1895d3d77c15ac29
hugopukito <gogo26hugop@gmail.com>
2023-07-03 17:09:24 +0200

    start adding form

--- TODO.md
+++ TODO.md
@@ -1,10 +1,3 @@
-# Menu
-
-- Conciergerie
-- Nos services
-- Formulaire
-- Contact
-
 # Formulaire
 
 - Type de bien
@@ -13,5 +6,6 @@
 -> 15m2 à 200m2 et plus
 - État du bien
 -> À rénover ou À décorer ou Prêt à louer
-- Adresse mail + n° de tél
+- Coordonnées
+-> Adresse mail + n° de tél
 - Champ libre de note text area
--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 
 function toggleNavBar() {
-  var element = document.getElementById('nav')
-  element.classList.toggle('show')
+  var nav = document.getElementById('nav')
+  nav.classList.toggle('show')
   document.body.classList.toggle('hide-scrollbar');
 }
 
-function clickedNavBar() {
-  var element = document.getElementById('nav')
-  element.classList.toggle('show')
+function openForm() {
+  toggleNavBar()
+  var form = document.getElementById('container-form')
+  form.classList.add('show')
+  document.body.classList.add('hide-scrollbar');
+
+  var menuButton = document.getElementById('menu-button')
+  menuButton.style.cursor = 'auto';
+  menuButton.onclick = null;
+}
+
+function closeForm() {
+  var form = document.getElementById('container-form')
+  form.classList.remove('show')
   document.body.classList.remove('hide-scrollbar');
+
+  var menuButton = document.getElementById('menu-button')
+  menuButton.style.cursor = 'pointer';
+  menuButton.onclick = toggleNavBar;
 }
