84e761f700d0f83e849dd59268b7177babcd04ff
hugopukito <gogo26hugop@gmail.com>
2023-09-17 23:14:20 +0200

    add img scroll auto

--- app.js
+++ app.js
@@ -126,3 +126,23 @@ function closeForm() {
   document.body.style.overflow = "visible"
   document.documentElement.style.overflow = "visible"
 }
+
+let currentIndex = 0;
+const images = document.querySelectorAll('.fade');
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    if (i === index) {
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  });
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+setInterval(nextImage, 4000);
